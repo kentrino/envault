a7ae4295312946e2be8dbb548e31d19ef5e0e6bc
kentrino <kentrino@h-kento.jp>
2024-01-01 18:53:42 +0900

    :white_check_mark: add test for env and with_env_vars helper

--- config/src/env.rs
+++ config/src/env.rs
@@ -1,13 +1,58 @@
 use crate::error::ConfigError;
 
-const PREFIX: &str = "ENV_KEY__";
+const PREFIX: &str = "ENV_KEY";
+
 
 pub fn key_for(_env: &str, _key: &str) -> Result<String, ConfigError> {
-    match std::env::var(PREFIX.to_string() + _env + "__" + _key) {
+    match std::env::var(PREFIX.to_string() + "__" + _env + "__" + _key) {
+        Ok(value) => return Ok(value),
+        Err(_) => (),
+    }
+    match std::env::var(PREFIX.to_string() + "__" + _env) {
+        Ok(value) => return Ok(value),
+        Err(_) => (),
+    }
+    match std::env::var(PREFIX.to_string()) {
         Ok(value) => Ok(value),
-        Err(_) => match std::env::var(PREFIX.to_string() + _env) {
-            Ok(value) => Ok(value),
-            Err(_) => Err(ConfigError::KeyNotFound),
-        },
+        Err(_) => Err(ConfigError::KeyNotFound),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::env::key_for;
+    use crate::with_env_vars::with_env_vars;
+
+    #[test]
+    fn with_root_password() {
+        with_env_vars(vec![
+            ("ENV_KEY", Some("password1"))
+        ], || {
+            let password = key_for("dev", "a").unwrap();
+            assert_eq!(password, "password1");
+        })
+    }
+
+    #[test]
+    fn with_password_for_env() {
+        with_env_vars(vec![
+            ("ENV_KEY", Some("password1")),
+            ("ENV_KEY__dev", Some("password2"))
+        ], || {
+            let password = key_for("dev", "a").unwrap();
+            assert_eq!(password, "password2");
+        })
+    }
+
+    #[test]
+    fn with_password_for_key() {
+        with_env_vars(vec![
+            ("ENV_KEY", Some("password1")),
+            ("ENV_KEY__dev", Some("password2")),
+            ("ENV_KEY__dev__a", Some("password3"))
+        ], || {
+            let password = key_for("dev", "a").unwrap();
+            assert_eq!(password, "password3");
+        })
     }
 }
--- config/src/lib.rs
+++ config/src/lib.rs
@@ -2,3 +2,4 @@ mod config;
 mod env;
 mod error;
 mod file;
+mod with_env_vars;
