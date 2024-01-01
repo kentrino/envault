50083aad408f55a81ec42587c5ff8059d9a6c1e7
kentrino <kentrino@h-kento.jp>
2024-01-01 18:54:41 +0900

    :tada: introduce clap

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,5 +1,32 @@
-use cipher::add;
+use std::process::exit;
+use clap::{Parser, Subcommand};
+
+
+#[derive(Parser)]
+#[command()]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// export env
+    Export {
+        #[arg(short, long, help = "Environment name")]
+        env: String
+    }
+}
 
 fn main() {
-    println!("2 + 2 = {}", add(2, 2));
+    let cli = Cli::parse();
+    match cli.command {
+        Some(Commands::Export { env }) => {
+            println!("export {}", env);
+        }
+        None => {
+            println!("No command specified");
+            exit(1);
+        }
+    }
 }
